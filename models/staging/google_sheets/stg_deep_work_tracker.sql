482dce7890a37cfe9b85636e3acb893986491bbc
brown5628 <brown5628@gmail.com>
2020-07-11 08:09:33 -0400

    create staging view

---
+++ models/staging/google_sheets/stg_deep_work_tracker.sql
@@ -0,0 +1,34 @@
+with 
+
+raw_data as (
+
+    select * from {{ source('google_sheets', 'dw_log') }}
+
+),
+
+cleaned as (
+
+    select 
+        {{ dbt_utils.surrogate_key(
+            [
+                '__sdc_row',
+                '__sdc_spreadsheet_id'
+            ]
+        ) }} as tracker_activity_id,
+        
+        type as activity_type,
+        subtype as activity_subtype,
+        minutes as activity_minutes,
+        date as activity_date,
+
+        _sdc_batched_at as el_batched_at,
+        _sdc_extracted_at as el_extracted_at,
+        _sdc_received_at as el_recieved_at,
+        _sdc_sequence as el_sequence,
+        _sdc_table_version as el_table_version
+    
+    from raw_data
+
+)
+
+select * from cleaned
