0029de4a5a759bc2ba9dfd8ac0feb66355ddf942
brown5628 <brown5628@gmail.com>
2020-08-09 09:39:08 -0400

    Extend hour spine a day into the future

--- models/staging/utils/hour_date_spine.sql
+++ models/staging/utils/hour_date_spine.sql
@@ -4,6 +4,6 @@
 {{ dbt_utils.date_spine(
     datepart="hour",
     start_date="to_date('06/01/2019', 'mm/dd/yyyy')",
-    end_date="current_date"
+    end_date="current_date + interval '1 day'"
    )
 }}
