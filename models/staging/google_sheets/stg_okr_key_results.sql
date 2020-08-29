79cf159c78191ed42f92629473180c4453b224aa
brown5628 <brown5628@gmail.com>
2020-08-29 09:20:02 -0400

    add key results

---
+++ models/staging/google_sheets/stg_okr_key_results.sql
@@ -0,0 +1,27 @@
+with 
+
+raw_data as (
+
+    select * from {{ source('raw_google_sheets_okrs', 'key_results')}}
+
+),
+
+cleaned as (
+
+    select 
+        key_result_id,
+        objective_id,
+        
+        key_result_text,
+        key_result_value,
+        key_result_unit,
+
+        to_date(active_from, 'YYYY-MM-DD') as date_active_from,
+        to_date(active_to, 'YYYY-MM-DD') as date_active_to,
+        to_date(key_result_target_date, 'YYYY-MM-DD') as date_key_result_target
+
+    from raw_data 
+
+)
+
+select * from cleaned
