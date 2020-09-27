8430e4fd9b000aa7322bad7dafee87eceee09cb3
brown5628 <brown5628@gmail.com>
2020-09-27 09:07:12 -0400

    add goodreads staging

---
+++ models/staging/google_sheets/stg_goodreads.sql
@@ -0,0 +1,25 @@
+with 
+
+raw_data as (
+
+    select * from {{ source('raw_goodreads', 'export') }}
+
+),
+
+cleaned as (
+
+    select 
+        "Book Id" as book_id,
+        "Title" as book_title,
+        "Author" as author_name,
+        "Exclusive Shelf" as reading_status,
+
+        to_date("Date Added", 'YYYY-MM-DD') as date_added,
+        
+        date '1899-12-30' + "Date Read"::int * interval '1' day as date_read
+
+    from raw_data
+
+)
+
+select * from cleaned
