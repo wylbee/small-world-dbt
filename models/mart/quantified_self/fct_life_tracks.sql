763d2af00f98c43a02a1d89c101c640135f3d0ea
brown5628 <brown5628@gmail.com>
2020-09-27 09:37:39 -0400

    Add to life track fcts

--- models/mart/quantified_self/fct_life_tracks.sql
+++ models/mart/quantified_self/fct_life_tracks.sql
@@ -6,13 +6,38 @@ tracks as (
 
 ),
 
-selected as (
+books as (
 
-    select
-        *
+    select * from {{ ref('stg_goodreads') }}
+),
 
-    from tracks
+books_cleaned as (
+
+    select 
+        tracker_id,
+        'book_read' as tracker_name,
+        'general' as tracker_context,
+        book_title as tracker_note,
+
+        1 as tracker_value,
+
+        'books' as tracker_units,
+        date_read as date_completed
+    
+    from books
+
+    where reading_status = 'read'
+
+),
+
+unioned as (
+
+    select * from tracks
+
+    union all 
+
+    select * from books_cleaned
 
 )
 
-select * from selected
+select * from unioned
