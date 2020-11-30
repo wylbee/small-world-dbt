8b1515d17488526984ae13648eff6deea811305d
brown5628 <brown5628@gmail.com>
2020-11-30 08:14:12 -0500

    flip toggl data from csv to api

--- models/staging/google_sheets/stg_toggl.sql
+++ models/staging/google_sheets/stg_toggl.sql
@@ -2,7 +2,7 @@ with
 
 raw_data as (
 
-    select * from {{ source('raw', 'toggl') }}
+    select * from {{ source('raw', 'toggl_api') }}
 
 ),
 
@@ -11,22 +11,21 @@ cleaned as (
     select
         {{ dbt_utils.surrogate_key(
             [
-                '"Start date"',
-                '"Start time"',
-                '"End date"',
-                '"End time"'
+                '"id"'
             ]
         ) }} as tracker_id,
 
-        "Client" as client_name,
-        "Project" as project_name,
-        "Description" as task_description,
-        to_date("Start date", 'YYYY-MM-DD') as date_started,
-        "Start time" :: time as time_started,
-        to_date("End date", 'YYYY-MM-DD') as date_ended,
-        "End time" :: time  as time_ended,
-        left("Duration",2) :: int * 3600 + substring("Duration",4,2) :: int * 60 + substring("Duration",7,2) :: int as duration_seconds,
-        "Tags" as context_tags
+        "id" as toggl_record_id,
+        "pid" as toggl_project_id,
+
+        "client" as client_name,
+        "project" as project_name,
+        "description" as task_description,
+        to_timestamp("start", 'YYYY-MM-DD hh24:mi:ss') :: timestamptz as date_started,
+        to_timestamp("end", 'YYYY-MM-DD hh24:mi:ss') :: timestamptz as date_ended,
+        to_timestamp("updated", 'YYYY-MM-DD hh24:mi:ss') :: timestamptz as date_last_updated,
+        "dur"/1000 :: int as duration_seconds,
+        to_json("tags"::text) #>> '{}' as context_tags
     
     from raw_data
 
